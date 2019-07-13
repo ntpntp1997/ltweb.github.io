ccc540c1431a9cb23cb6740e01b20b84852c222e
ntpntp1997 <50309418+ntpntp1997@users.noreply.github.com>
2019-07-14 00:20:25 +0700

    afwsa

--- live2d/js/message.js
+++ live2d/js/message.js
@@ -23,12 +23,12 @@ String.prototype.renderTip = function (context) {
 var re = /x/;
 console.log(re);
 re.toString = function() {
-    showMessage('哈哈，你打开了控制台，是想要看看我的秘密吗？', 5000);
+    showMessage('Haha, bạn đã mở giao diện điều khiển, bạn có muốn xem bí mật của tôi không？', 5000);
     return '';
 };
 
 $(document).on('copy', function (){
-    showMessage('你都复制了些什么呀，转载要记得加上出处哦~~', 5000);
+    showMessage('Bạn đã sao chép những gì? In lại để nhớ thêm nguồn~~', 5000);
 });
 
 function initTips(){
@@ -66,36 +66,36 @@ initTips();
         text = '嗨！来自 <span style="color:#0099cc;">' + referrer.hostname + '</span> 的朋友！';
         var domain = referrer.hostname.split('.')[1];
         if (domain == 'baidu') {
-            text = '嗨！ 来自 百度搜索 的朋友！<br>欢迎访问<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
+            text = 'Này! Bạn bè từ tìm kiếm của Baidu! <br> Chào mừng bạn đến thăm<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
         }else if (domain == 'so') {
             text = '嗨！ 来自 360搜索 的朋友！<br>欢迎访问<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
         }else if (domain == 'google') {
-            text = '嗨！ 来自 谷歌搜索 的朋友！<br>欢迎访问<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
+            text = 'Này! Bạn bè từ Google Tìm kiếm!<br>Chào mừng đến thăm<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
         }
     }else {
         if (window.location.href == `${home_Path}`) { //主页URL判断，需要斜杠结尾
             var now = (new Date()).getHours();
             if (now > 23 || now <= 5) {
-                text = '你是夜猫子呀？这么晚还不睡觉，明天起的来嘛？';
+                text = 'Bạn có phải là một con cú đêm? Tôi vẫn không ngủ quá muộn, đến ngày mai.？';
             } else if (now > 5 && now <= 7) {
-                text = '早上好！一日之计在于晨，美好的一天就要开始了！';
+                text = 'Chào buổi sáng Một ngày là vào buổi sáng, và một ngày tốt lành sắp bắt đầu！';
             } else if (now > 7 && now <= 11) {
-                text = '上午好！工作顺利嘛，不要久坐，多起来走动走动哦！';
+                text = 'Chào buổi sáng Làm việc tốt, đừng ngồi lâu, đứng dậy và đi bộ xung quanh！';
             } else if (now > 11 && now <= 14) {
-                text = '中午了，工作了一个上午，现在是午餐时间！';
+                text = 'Buổi trưa, tôi làm việc cho một buổi sáng, bây giờ là giờ ăn trưa！';
             } else if (now > 14 && now <= 17) {
-                text = '午后很容易犯困呢，今天的运动目标完成了吗？';
+                text = 'Nó rất dễ chán vào buổi chiều. Mục tiêu của môn thể thao ngày hôm nay đã hoàn thành chưa？';
             } else if (now > 17 && now <= 19) {
-                text = '傍晚了！窗外夕阳的景色很美丽呢，最美不过夕阳红~~';
+                text = 'Nó muộn quá! Phong cảnh hoàng hôn ngoài cửa sổ rất đẹp, đẹp nhất nhưng hoàng hôn đỏ~~';
             } else if (now > 19 && now <= 21) {
-                text = '晚上好，今天过得怎么样？';
+                text = 'Chào buổi tối, hôm nay bạn thế nào？';
             } else if (now > 21 && now <= 23) {
-                text = '已经这么晚了呀，早点休息吧，晚安~~';
+                text = 'Nó đã quá muộn, nghỉ ngơi sớm, chúc ngủ ngon~~';
             } else {
-                text = '嗨~ 快来逗我玩吧！';
+                text = 'Này ~ Hãy đến và trêu chọc tôi！';
             }
         }else {
-            text = '欢迎阅读<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
+            text = 'Chào mừng bạn đến đọc<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
         }
     }
     showMessage(text, 12000);
